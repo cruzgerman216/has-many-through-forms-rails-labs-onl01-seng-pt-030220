7b99d7a7ea25735e74198522de29c803d905d3ac
German Cruz <cruzgerman216@yahoo.com>
2020-08-08 01:21:13 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  
+
   def categories_attributes=(categories_hashes)
     categories_hashes.each do |i, category_attributes|
       category = self.categories.find_or_create_by(name: category_attributes[:name])
       if !self.categories.include?(category)
       self.post_categories.build(:category => category)
     end
-    
+
     end
 end
